624d7c21e1df2bb769b57fff4ca3d87631354303
Sepx <sepx@Sepxs.local>
2025-08-20 10:12:59 +0700

    system test toggle checkbox

--- test/system/quests_spec.rb
+++ test/system/quests_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe "Quests Management", type: :system do
     click_button "Create Quest"
     expect(page).to have_content("New Quest via Capybara")
   end
+
+  it "toggles quest status" do
+    visit quests_path
+    quest_checkbox = find("#quest_status_#{@quest.id}")
+    original_checked = quest_checkbox.checked?
+
+    quest_checkbox.click
+
+    if original_checked
+      expect(page).to have_unchecked_field("quest_status_#{@quest.id}")
+    else
+      expect(page).to have_checked_field("quest_status_#{@quest.id}")
+    end
+  end
 end
